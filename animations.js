9fefaeb40852559db56baf92bc483ab6cbd3b2fe
ama-coder <67456142+ama-coder@users.noreply.github.com>
2022-02-05 20:55:48 +0330

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,10 @@
+function addHover(elm = document.createElement('img')) {
+
+        elm.classList.add('hover');
+
+}
+function removeHover(elm = document.createElement('img')) {
+
+        elm.classList.remove('hover');
+
+}
